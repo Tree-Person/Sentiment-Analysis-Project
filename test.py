5c82a94970216f3c2181d0324ae13637ed988afc
Tree-Person <cristhianwmontiel@gmail.com>
2023-12-07 21:53:29 -0800

    test

--- test.py
+++ test.py
@@ -2,25 +2,38 @@ from classes import *
 from functions import *
 
 
-water = SentimentAnalyzer("water")
-dfs = water.getDf()
 
-info = []
-for i in dfs['Score']:
-        if i >= 1:
-            info.append('positive')
-        elif 0 < i < 1:
-            info.append('neutral')
-        else:
-            info.append('negative')
+def test_find_highest_element():
+    lst1 = [1, 2, 3, 4, 5]
+    expected1 = (5, 4)
+    assert find_highest_element(lst1) == expected1
 
-dfs['Results'] = info
+    lst2 = [-5, -4, -3, -2, -1]
+    expected2 = (-1, 4)
+    assert find_highest_element(lst2) == expected2
 
-def find_highest_element(lst):
-    max_ele = max(lst)
-    for i , x in enumerate(lst):
-        if x == max_ele:
-            return max_ele , i
-        
-lst = [1, 2, 3, 4, 5]
-print(find_highest_element(lst))
+    lst3 = [-2, 0, 3, -5, 2]
+    expected3 = (3, 2)
+    assert find_highest_element(lst3) == expected3
+
+    lst4 = [1, 2, 3, 3, 5]
+    expected4 = (5, 4)
+    assert find_highest_element(lst4) == expected4
+
+    print("All test cases passed!")
+   
+def test_sentiment_analyzer():
+    topic = "example"
+    analyzer = SentimentAnalyzer(topic)
+
+    # Test getDf method
+    assert isinstance(analyzer.getDf(), pd.DataFrame)
+    print("getDf method test passed!")
+
+    # Test getMost method
+    assert isinstance(analyzer.getMost(), str)
+    print("getMost method test passed!")
+
+
+test_find_highest_element()
+test_sentiment_analyzer()
